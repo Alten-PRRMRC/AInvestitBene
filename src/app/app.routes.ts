01a8ab8e4a7362ef7da7a297ad1398c3d547fd78
Alten-PRRMRC <marco.perrelli@alten.it>
2025-07-24 18:55:31 +0200

    feat: not-found configured in routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '**',
+    loadComponent: () => import('./not-found/not-found.component').then(m => m.NotFoundComponent),
+    title: 'Oops'
+  }
+];
+
