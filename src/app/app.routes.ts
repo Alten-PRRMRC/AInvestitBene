fa53c061ee9e2c910d1cea94b065a5ed7d52a30a
Alten-PRRMRC <marco.perrelli@alten.it>
2025-07-25 16:25:37 +0200

    feat: expense component configured in routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,11 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: '',
+    loadComponent: () => import('./expense/expense.component').then(m => m.ExpenseComponent),
+    title: 'Expense List'
+  },
+  {
     path: '**',
     loadComponent: () => import('./not-found/not-found.component').then(m => m.NotFoundComponent),
     title: 'Oops'
