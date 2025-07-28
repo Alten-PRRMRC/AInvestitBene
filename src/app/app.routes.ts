ea7eb942b3ab80cd648f1168142f31e5de702bc3
Alten-PRRMRC <marco.perrelli@alten.it>
2025-07-28 18:58:57 +0200

    feat: added expenseRoutes and canMatch guard, also auth component as fallback; refactor: better format in app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,27 @@
-import { Routes } from '@angular/router';
+import type { Routes } from "@angular/router";
+import { routes as expenseRoutes } from "./expense/expense.routes";
+import { canMatchGuard } from "./auth/can-match.guard";
 
 export const routes: Routes = [
-  {
-    path: '',
-    loadComponent: () => import('./expense/expense.component').then(m => m.ExpenseComponent),
-    title: 'Expense List'
-  },
-  {
-    path: '**',
-    loadComponent: () => import('./not-found/not-found.component').then(m => m.NotFoundComponent),
-    title: 'Oops'
-  }
+	{
+		path: "",
+		loadComponent: () =>
+			import("./expense/expense.component").then((m) => m.ExpenseComponent),
+		children: expenseRoutes,
+		canMatch: [canMatchGuard],
+		title: "Expense List",
+	},
+	{
+		path: "",
+		loadComponent: () =>
+			import("./auth/auth.component").then((m) => m.AuthComponent),
+	},
+	{
+		path: "**",
+		loadComponent: () =>
+			import("./not-found/not-found.component").then(
+				(m) => m.NotFoundComponent,
+			),
+		title: "Oops",
+	},
 ];
-
