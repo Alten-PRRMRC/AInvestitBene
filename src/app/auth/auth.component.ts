ac7e768552e3b3126b00ffb9d759a0c7e00e4ef8
Alten-PRRMRC <marco.perrelli@alten.it>
2025-07-28 19:38:01 +0200

    feat: added dummy button to set AUTH_TOKEN in auth style: configured auth css

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from "@angular/core";
+import { LocalstorageService } from "../localstorage/localstorage.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-auth',
-  imports: [],
-  templateUrl: './auth.component.html',
-  styleUrl: './auth.component.css'
+	selector: "app-auth",
+	imports: [],
+	templateUrl: "./auth.component.html",
+	styleUrl: "./auth.component.css",
 })
 export class AuthComponent {
+	private localstorage: LocalstorageService = inject(LocalstorageService);
+	private router: Router = inject(Router);
 
+	auth() {
+		this.localstorage.setItem<string>("AUTH_TOKEN", "dummy");
+		this.router.navigate([""]);
+	}
 }
