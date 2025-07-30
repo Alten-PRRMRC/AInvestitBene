f5ed5ad0c7320c57d209291800dc133d8bba8c62
Alten-PRRMRC <marco.perrelli@alten.it>
2025-07-30 20:10:54 +0200

    Auto-generated commit

--- chunk-BYCU6YFR.js
+++
@@ -1,1 +0,0 @@
-import{Fa as o,Ga as s,Ja as i,ra as n}from"./chunk-ZFZ5SDHC.js";var p=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=n({type:t,selectors:[["expense-list"]],decls:2,vars:0,template:function(e,a){e&1&&(o(0,"p"),i(1,"list works!"),s())},encapsulation:2})};export{p as ListComponent};
