1dfea88f71800d064ffec68c89b48a62a2bb0396
Alten-PRRMRC <marco.perrelli@alten.it>
2025-08-01 23:01:55 +0200

    feat: added list-switch to the list component

--- src/app/expense/list/list.component.ts
+++ src/app/expense/list/list.component.ts
@@ -3,27 +3,36 @@
 import { CommonModule } from "@angular/common";
 import { Component, inject, OnInit } from "@angular/core";
 import { RouterLink } from "@angular/router";
-import { Observable } from "rxjs";
+import { BehaviorSubject, Observable } from "rxjs";
 import { Expense } from "../expense.model";
 import { ExpenseService } from "../expense.service";
 import { CategoryPipe } from "./category.pipe";
+import { ListSwitchComponent } from './switch/list-switch.component';
 
 /**
  * Component that shows a table of users transactions
  */
 @Component({
-	selector: "expense-list",
-	imports: [CommonModule, RouterLink, CategoryPipe],
-	templateUrl: "./list.component.html",
-	styleUrl: "./list.component.css",
+  selector: "expense-list",
+  imports: [CommonModule, RouterLink, CategoryPipe, ListSwitchComponent],
+  templateUrl: "./list.component.html",
+  styleUrl: "./list.component.css",
 })
 export class ListComponent implements OnInit {
-	/**
-	 * Initialized injecting ExpenseService service
-	 * @private
-	 * @see ExpenseService
-	 */
-	private expenseService: ExpenseService = inject(ExpenseService);
+
+  /**
+   * BehaviorSubject that stores and emits checkbox values from ListSwitchComponent.
+   * If is true is annually, false is monthly
+   * @see ListSwitchComponent
+   */
+  checkboxValue$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+
+  /**
+   * Initialized injecting ExpenseService service
+   * @private
+   * @see ExpenseService
+   */
+  private expenseService: ExpenseService = inject(ExpenseService);
 
 	/**
 	 * List of table title based on Expense
@@ -40,12 +49,22 @@ export class ListComponent implements OnInit {
 		this.expenses$ = this.expenseService.getItems();
 	}
 
-	/**
-	 * Delete from the table an expense item by its ID calling `deleteItem()` of the ExpenseService.
-	 * @param id - The ID of the expense item to be deleted
-	 * @see ExpenseService
-	 */
-	deleteExpense(id: string): void {
-		this.expenseService.deleteItem(id);
-	}
+  /**
+   * Delete from the table an expense item by its ID calling `deleteItem()` of the ExpenseService.
+   * @param id - The ID of the expense item to be deleted
+   * @see ExpenseService
+   */
+  deleteExpense(id: string): void {
+    this.expenseService.deleteItem(id);
+  }
+
+  /**
+   * Change the value of the checkboxValue$ to the current status of the checkbox
+   * @param value - Boolean of the checkbox
+   * @see ListSwitchComponent
+   * @see checkboxValue$
+   */
+  onCheckboxChange(value: boolean): void {
+    this.checkboxValue$.next(value);
+  }
 }
