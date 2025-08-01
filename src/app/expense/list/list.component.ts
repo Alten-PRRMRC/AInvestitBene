6af1d87ebd8d8f5f0fb8ccb9dd6c0b44393e3475
Alten-PRRMRC <marco.perrelli@alten.it>
2025-08-02 00:33:05 +0200

    feat: shows tables grouped by month or year refactor: reordered class fields docs: added docs

--- src/app/expense/list/list.component.ts
+++ src/app/expense/list/list.component.ts
@@ -3,11 +3,12 @@
 import { CommonModule } from "@angular/common";
 import { Component, inject, OnInit } from "@angular/core";
 import { RouterLink } from "@angular/router";
-import { BehaviorSubject, Observable } from "rxjs";
+import { BehaviorSubject, combineLatest, map, Observable } from "rxjs";
 import { Expense } from "../expense.model";
 import { ExpenseService } from "../expense.service";
 import { CategoryPipe } from "./category.pipe";
 import { ListSwitchComponent } from './switch/list-switch.component';
+import { Dict as DictExpense } from './dict.model';
 
 /**
  * Component that shows a table of users transactions
@@ -21,6 +22,15 @@ import { ListSwitchComponent } from './switch/list-switch.component';
 export class ListComponent implements OnInit {
 
   /**
+   * Initialized injecting ExpenseService service
+   * @private
+   * @see ExpenseService
+   */
+  private expenseService: ExpenseService = inject(ExpenseService);
+
+  protected readonly Object = Object; // Used to check if expenses$ is not a dict empty
+
+  /**
    * BehaviorSubject that stores and emits checkbox values from ListSwitchComponent.
    * If is true is annually, false is monthly
    * @see ListSwitchComponent
@@ -28,26 +38,54 @@ export class ListComponent implements OnInit {
   checkboxValue$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
   /**
-   * Initialized injecting ExpenseService service
-   * @private
-   * @see ExpenseService
+   * List of table title based on Expense
+   * @see Expense
    */
-  private expenseService: ExpenseService = inject(ExpenseService);
+  titleTable: string[] = ["Description", "Amount", "Category", "Date", ""];
 
-	/**
-	 * List of table title based on Expense
-	 * @see Expense
-	 */
-	titleTable: string[] = ["Description", "Amount", "Category", "Date", ""];
+  /**
+   * Observable of a dictionary Expense[]
+   * @see DictExpense
+   * @see Expense
+   */
+  expenses$!: Observable<DictExpense<Expense[]>>;
 
-	/**
-	 * Observable of a list Expense
-	 */
-	public expenses$!: Observable<Expense[]>;
+  /**
+   * Observable of dictionary keys
+   * @see DictExpense
+   */
+  expensesKey$!: Observable<string[]>;
 
-	ngOnInit(): void {
-		this.expenses$ = this.expenseService.getItems();
-	}
+  ngOnInit(): void {
+    this.expenses$ = combineLatest([ // To create a new observable dictionary we combine the follow observables.
+      this.expenseService.getItems(), // An observable of Expense from the expense service.
+      this.checkboxValue$.asObservable()] // An observable of booleans from the list-switch checkbox.
+    ).pipe( // Each element of this observable will be
+      map(([expenses, checkbox]: [Expense[], boolean]): DictExpense<Expense[]> => { // transformed in a dictionary.
+        const obj: DictExpense<Expense[]> = {}; // Initialize an empty dictionary.
+        for(let i: number = 0; i < expenses.length; i++) { // For each Expense[] items
+          let key: string = checkbox ? // the key will be, if checkbox
+            expenses[i].date.getFullYear().toString() : // is true, it's Expense's date annual.
+            expenses[i].date.toLocaleString('default', { month: 'long' }); // Otherwise, it's Expense's date month
+          if (!obj[key]) { // If the current key does not exist in the dictionary
+            obj[key] = []; // initialize an empty array
+          }
+          obj[key].push(expenses[i]); // and adds the current Expense to the Expense's array of dictionary
+        }
+        return obj; // Return populated Expense's dictionary
+      }),
+    );
+    // Now, we need an observable dictionary keys of observable Expense's dictionary to determinate the order to show it.
+    this.expensesKey$ = this.expenses$.pipe( // Each element of Expense's dictionary will be
+      map((obj: DictExpense<Expense[]>): string[] => { // transformed in a dictionary keys.
+        const keys: string[] = Object.keys(obj); // Get all the keys from Expense's dictionary and sort them
+        return this.checkboxValue$.value ? // If checkbox
+          keys.sort((a: string, b: string): number =>
+            parseInt(a) - parseInt(b)) : // is true, annually sorted decrescent.
+            keys.sort() // Otherwise false, monthly sorted alphabetically.
+      })
+    )
+  }
 
   /**
    * Delete from the table an expense item by its ID calling `deleteItem()` of the ExpenseService.
