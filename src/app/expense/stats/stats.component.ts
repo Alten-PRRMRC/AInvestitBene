11551b3e6f7b23aa71649bda50cf193e9b8193f6
Alten-PRRMRC <marco.perrelli@alten.it>
2025-08-07 09:54:15 +0200

    feat: stats component generated

---
+++ src/app/expense/stats/stats.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'expense-stats',
+  imports: [],
+  templateUrl: './stats.component.html',
+  styleUrl: './stats.component.css'
+})
+export class StatsComponent {
+
+}
