0c1a0855101b346fbfaa4b130ac752e7d5cb553a
Alten-PRRMRC <marco.perrelli@alten.it>
2025-08-07 09:53:42 +0200

    fix: import path of app-highlight-big directive

--- src/app/expense/list/list.component.ts
+++ src/app/expense/list/list.component.ts
@@ -10,7 +10,7 @@ import { CategoryPipe } from "./category.pipe";
 import { ListSwitchComponent } from './switch/list-switch.component';
 import { Dict as DictExpense } from './dict.model';
 import { ListSearchComponent } from './search-field/list-search.component';
-import { AppHighlightBig } from '../../app/expense/list/app-highlight-big';
+import { AppHighlightBig } from '../list/app-highlight-big';
 
 /**
  * Component that shows a table of users transactions
