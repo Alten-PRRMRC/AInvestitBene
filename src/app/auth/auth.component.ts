a9d9f2d1fc6c9445c4f66c58d033208fc4b18d10
Alten-PRRMRC <marco.perrelli@alten.it>
2025-08-09 10:01:28 +0200

    docs: added docs in auth

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,7 +1,11 @@
+// Import required modules
 import { Component, inject } from "@angular/core";
 import { Router } from "@angular/router";
 import { LocalstorageService } from "../localstorage/localstorage.service";
 
+/**
+ * Component that provides a user interface for login and registration.
+ */
 @Component({
 	selector: "app-auth",
 	imports: [],
@@ -9,11 +13,34 @@ import { LocalstorageService } from "../localstorage/localstorage.service";
 	styleUrl: "./auth.component.css",
 })
 export class AuthComponent {
+  /**
+   * Injects localStorage service
+   * @see LocalstorageService
+   * @private
+   */
 	private localstorage: LocalstorageService = inject(LocalstorageService);
+
+  /**
+   * Injects Router service
+   * @see Router
+   * @private
+   */
 	private router: Router = inject(Router);
 
-	auth() {
-		this.localstorage.setItem<string>("AUTH_TOKEN", "dummy");
+  /**
+   * Key used for storing auth token in localStorage
+   * @private
+   */
+  private readonly STORAGE_KEY = "AUTH_TOKEN";
+
+  /**
+   * Initialize value in localStorage through the key and reload the page.
+   * @see STORAGE_KEY
+   * @see Router
+   * @see LocalstorageService
+   */
+  auth() {
+		this.localstorage.setItem<string>(this.STORAGE_KEY, "dummy");
 		this.router.navigate([""]);
 	}
 }
