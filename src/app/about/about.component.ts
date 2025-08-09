026916e2c5e6d3d85c3a0a781e13c6d057b82d81
Alten-PRRMRC <marco.perrelli@alten.it>
2025-08-09 09:05:14 +0200

    feat: in about page a button debug to reset local storage

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {LocalstorageService} from '../localstorage/localstorage.service';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+  private localStorageService = inject(LocalstorageService);
 
+  reset() {
+    this.localStorageService.clear();
+  }
 }
