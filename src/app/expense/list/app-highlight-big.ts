506c2c187fd1be29f4885fa637f2654406f2ab16
Alten-PRRMRC <marco.perrelli@alten.it>
2025-09-03 17:52:49 +0200

    refactor: renamed `appAppHighlightBig` to `AppHighlightBig`; AppHighlightBig directive now use reactive effect and Renderer2

--- src/app/expense/list/app-highlight-big.ts
+++ src/app/expense/list/app-highlight-big.ts
@@ -1,32 +1,63 @@
-import {AfterViewInit, Directive, ElementRef, inject, input, InputSignal, OnDestroy} from '@angular/core';
-
+import {
+	Directive,
+	effect,
+	ElementRef,
+	inject,
+	input,
+	InputSignal,
+	OnDestroy,
+	Renderer2,
+} from "@angular/core";
 /**
  * Custom Directive that check if value is major of assigned import and add css class
  * @see limitImport
  */
 @Directive({
-  selector: '[appAppHighlightBig]'
+	selector: "[AppHighlightBig]",
 })
-export class AppHighlightBig implements OnDestroy, AfterViewInit {
+export class AppHighlightBig implements OnDestroy {
+	/**
+	 * Element which this directive is assigned
+	 * @private
+	 */
+	private el: ElementRef = inject(ElementRef);
 
-  /**
-   * Limit of the value to check
-   */
-  limitImport: InputSignal<number> = input(500);
+	private render: Renderer2 = inject(Renderer2);
 
-  /**
-   * Element which this directive is assigned
-   * @private
-   */
-  private el: ElementRef = inject(ElementRef);
-  ngAfterViewInit(): void {
-    let elImport: string = this.el.nativeElement.innerHTML.substring(1).replace(",", "").split(".");
-    this.el.nativeElement.innerHTML = Number(elImport[0]) > this.limitImport() ?
-      `<b class="badge badge-dash badge-warning bg-warning text-warning-content">${this.el.nativeElement.innerHTML}</b>` :
-      this.el.nativeElement.innerHTML;
-  }
+	/**
+	 * Limit of the value to check
+	 */
+	limitImport: InputSignal<number> = input(500);
 
-  ngOnDestroy() {
-    this.el.nativeElement.innerHTML = '';
-  }
+	/**
+	 * Value to confront with limit
+	 */
+	amountImport: InputSignal<number> = input.required<number>();
+
+	/**
+	 * Currency type to display the amount
+	 */
+	currencyImport: InputSignal<string> = input("EUR");
+
+	constructor() {
+		effect(() => {
+			const limit = this.limitImport();
+			const value = this.amountImport();
+			const currency = this.currencyImport();
+			const formatted = new Intl.NumberFormat("en-UK", {
+				style: "currency",
+				currency: currency,
+			}).format(value);
+
+			let html =
+				value > limit
+					? `<b class="badge badge-dash badge-warning bg-warning text-warning-content">${formatted}</b>`
+					: formatted;
+			this.render.setProperty(this.el.nativeElement, "innerHTML", html);
+		});
+	}
+
+	ngOnDestroy() {
+		this.el.nativeElement.innerHTML = "";
+	}
 }
